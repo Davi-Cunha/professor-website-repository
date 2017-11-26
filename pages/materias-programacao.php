812eb8361e07f70599cff3bd26332d61c85f8e14
MANGA-ESTRANHA <davipccunhasilva@hotmail.com>
2017-11-26 19:22:02 -0200

    First Commit

---
+++ pages/materias-programacao.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Matérias Abordadas - Programação</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://use.fontawesome.com/f71dea074f.js"></script>
+        <link type="text/css" href="../css/style.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://cdn.bootcss.com/animate.css/3.5.1/animate.min.css">
+        <script type="text/javascript" src="../js/gerais.js"></script>
+        <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+    </head>
+    <body>
+        <nav class="nav-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h1>Professor</h1>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="menu">
+                            <ul>
+                                <li>
+                                    <a href="../index.php">Home</a>
+                                </li>
+                                <li>
+                                    <a href="../index.php#sobre">Sobre</a>
+                                </li>
+                                <li>
+                                    <a href="../index.php#contatos">Contatos</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+        <header class="title direito-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Programação</h1>
+                        <h2>Matérias Abordadas</h2>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section class="materias">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <article>
+                            <header>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#prog"><i class="fa fa-bug" aria-hidden="true"></i></button>
+                            </header>
+                            <div class="conteudo collapse" id="prog">
+                                <ul>
+                                    <li>C</li>
+                                    <li>C++</li>
+                                    <li>Java</li>
+                                    <li>Desenvolvimento Android</li>
+                                    <li>HTML5, CSS3, Bootstrap</li>
+                                    <li>Python</li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                            <header>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#hardware"><i class="fa fa-desktop" aria-hidden="true"></i></button>
+                            </header>
+                            <div class="conteudo collapse" id="hardware">
+                                <ul>
+                                    <li>Sistemas Digitais</li>
+                                    <li>Redes</li>
+                                    <li>Projetos com embarcados</li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!-- <section class="materias-carousel">
+            <div class="container-fluid">
+                <div class="row">
+                  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner" role="listbox">
+                          <div class="carousel-item">
+                            <a href="materias-direito.html"><img src="../img/direito-slider.jpg" alt="First Slide" /></a>
+                          <div class="carousel-caption">
+                            <h2>Direito</h2>
+                          </div>
+                          </div>
+
+                          <div class="carousel-item">
+                              <a href="materias-engenharia.html"><img src="../img/engenharia-slider.jpg" alt="Second Slide" /></a>
+                              <div class="carousel-caption">
+                                  <h2>Engenharia</h2>
+                              </div>
+                          </div>
+
+                          <div class="carousel-item active">
+                              <a><img src="../img/direito-slider-3.jpg" alt="Third Slide" /></a>
+                              <div class="carousel-caption">
+                                  <h2>Programação</h2>
+                              </div>
+                          </div>
+                      </div>
+
+                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                          <span class="icon-prev" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                      </a>
+
+                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                          <span class="icon-next" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                      </a>
+                  </div>
+              </div>
+            </div>
+        </section> -->
+
+
+
+        <footer>
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <h6>Coded with <i class="fa fa-heart red"></i> by <a href="https://github.com/MANGA-ESTRANHA" target="_blank">MANGA-ESTRANHA</a></h6>
+                </div>
+            </div>
+        </footer>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+
+    </body>
+</html>
