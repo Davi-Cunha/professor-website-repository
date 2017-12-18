181dde3cfff1f4f667936d4d3badd580599ca9ab
MANGA-ESTRANHA <davipccunhasilva@hotmail.com>
2017-12-18 14:50:02 -0200

    "Parallax effect" adicionado no title, aprimoração do código como todo. Adicionado as sections com includes de php.

--- pages/materias-engenharia.php
+++ pages/materias-engenharia.php
@@ -1,204 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>Matérias Abordadas - Engenharia\</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://use.fontawesome.com/f71dea074f.js"></script>
-        <link type="text/css" href="../css/style.css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="http://cdn.bootcss.com/animate.css/3.5.1/animate.min.css">
-        <script type="text/javascript" src="../js/gerais.js"></script>
-        <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-    </head>
-    <body>
-        <nav class="nav-top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h1>Professor</h1>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="menu">
-                            <ul>
-                                <li>
-                                    <a href="../index.php">Home</a>
-                                </li>
-                                <li>
-                                    <a href="../index.php#sobre">Sobre</a>
-                                </li>
-                                <li>
-                                    <a href="../index.php#contatos">Contatos</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
+<head>
+    <meta charset="utf-8">
+    <title>Matérias Abordadas - Engenharia\</title>
+    <?php include('../includes/link-paths.php'); ?>
+</head>
+<body>
 
+<?php include('../includes/navbar.php'); ?>
 
-        <header class="title direito-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1>Engenharia</h1>
-                        <h2>Matérias Abordadas</h2>
-                    </div>
-                </div>
-            </div>
-        </header>
+<?php include('../includes/title.php') ?>
 
-        <section class="materias">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3">
-                        <article>
-                            <header>
-                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#eng-civil"><i class="fa fa-building-o" aria-hidden="true"></i></button>
-                            </header>
-                            <div class="conteudo collapse" id="eng-civil">
-                                <ul>
-                                    <li>Mecânica Vetorial</li>
-                                    <li>Mecânica Geral</li>
-                                    <li>Hidráulica</li>
-                                    <li>Fenomenos de Transportes</li>
-                                    <li>Transferencia de Calor</li>
-                                    <li>Estruturas Metálicas</li>
-                                    <li>Concreto Armado</li>
-                                    <li>Ciência dos materiais e Tecnologia</li>
-                                    <li>Resistência dos materiais</li>
-                                    <li>Análise estrutural</li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article>
-                            <header>
-                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#eng-mec"><i class="fa fa-cogs" aria-hidden="true"></i></button>
-                            </header>
-                            <div class="conteudo collapse" id="eng-mec">
-                                <ul>
-                                    <li>Mecanica dos Fluidos e dos sólidos</li>
-                                    <li>Termodinâmica Básica e Aplicada</li>
-                                    <li>Cinemática e Dinâmica dos sólidos</li>
-                                    <li>Ciência dos materiais e Tecnologia</li>
-                                    <li>Elementos de máquinas</li>
-                                    <li>Teoria do Controle</li>
-                                    <li>Mecânica Fundamental eRacional</li>
-                                    <li>Soldagem, Usinagem</li>
-                                    <li>Processos de fabricação</li>
-                                    <li>Tratamentos Térmicos</li>
-                                    <li>Tratamentos Térmicos</li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article>
-                            <header>
-                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#adm-eco1"><i class="fa fa-money" aria-hidden="true"></i></button>
-                            </header>
-                            <div class="conteudo collapse" id="adm-eco1">
-                                <ul>
-                                    <li>Estatística I e II</li>
-                                    <li>Métodos quantitativos</li>
-                                    <li>Mercado de capitais</li>
-                                    <li>Matemática 1</li>
-                                    <li>Contabilidade 1 e 2</li>
-                                    <li>Orçamento empresarial</li>
-                                    <li>Pesquisa Operacional</li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article>
-                            <header>
-                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#adm-eco2"><i class="fa fa-money" aria-hidden="true"></i></button>
-                            </header>
-                            <div class="conteudo collapse" id="adm-eco2">
-                                <ul>
-                                    <li>(Cálculo, Física, Limites, Derivadas, Integrais)</li>
-                                    <li>Álgebra Vetorial</li>
-                                    <li>Matemática Basica e Financeira</li>
-                                    <li>Calculo 1, 2, 3, 4 e Numerico</li>
-                                    <li>GAAV (Geometria Analítica e Algebra Vetorial)</li>
-                                    <li>CVV (Calculo de Varias Variaveis)</li>
-                                    <li>Estatistica Básica e Aplicada</li>
-                                    <li>Geometria analítica e álgebra linear</li>
-                                    <li>Fisica 1/2/3/4 , Mecânica, Eletromagnetismo</li>
-                                    <li>Elétrica, Circuitos Elétricos</li>
-                                    <li>EDO (Equações Diferenciais)</li>
-                                    <li>Oscilações e ondas</li>
-                                    <li>Quimica Básica</li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </section>
+<?php include('../includes/../includes/materias.php') ?>
 
+<?php include('../includes/contatos.php'); ?>
 
+<?php include('../includes/footer.php'); ?>
 
-        <!-- <section class="materias-carousel">
-            <div class="container-fluid">
-                <div class="row">
-                  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                      <div class="carousel-inner" role="listbox">
-                          <div class="carousel-item">
-                            <a href="materias-direito.html"><img src="../img/direito-slider.jpg" alt="First Slide" /></a>
-                          <div class="carousel-caption">
-                            <h2>Direito</h2>
-                          </div>
-                          </div>
-
-                          <div class="carousel-item active">
-                              <a><img src="../img/engenharia-slider.jpg" alt="Second Slide" /></a>
-                              <div class="carousel-caption">
-                                  <h2>Engenharia</h2>
-                              </div>
-                          </div>
-
-                          <div class="carousel-item">
-                              <a href="materias-programacao.html"><img src="../img/direito-slider-3.jpg" alt="Third Slide" /></a>
-                              <div class="carousel-caption">
-                                  <h2>Programação</h2>
-                              </div>
-                          </div>
-                      </div>
-
-                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                          <span class="icon-prev" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                      </a>
-
-                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                          <span class="icon-next" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                      </a>
-                  </div>
-              </div>
-            </div>
-        </section> -->
-
-
-
-        <footer>
-            <div class="container">
-                <div class="col-md-10 col-md-offset-1 text-center">
-
-                    <h6>Coded with <i class="fa fa-heart red"></i> by <a href="https://github.com/MANGA-ESTRANHA" target="_blank">MANGA-ESTRANHA</a></h6>
-                </div>
-            </div>
-        </footer>
-
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
-
-    </body>
+</body>
 </html>
